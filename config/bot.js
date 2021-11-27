b6c5e1153b70559ae406ff3608dfcf7faea8c544
Leon <44817515+Ermo123@users.noreply.github.com>
2021-11-26 18:58:02 -0600

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,18 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':rotating_light:',
+        queue: ':notes:',
+        music: ':guitar:',
+        success: ':heavy_check_mark:',
+        natbutt: ':two_hearts:',
+    },
+
+    discord: {
+        token: 'OTEwMjY5ODU0NjEzOTA5NTI0.YZQY3Q.Q8hKgBcQ-UwxAs46WB2nFpJG868',
+        prefix: '&',
+        activity: 'Vibing to Spotify 📦🐋',
+    },
+
+    
+};
